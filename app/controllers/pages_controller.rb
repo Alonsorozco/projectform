ef177c252a17411a49ad7e83a47e97e6f331d00b
Alonsorozco <83556353+Alonsorozco@users.noreply.github.com>
2021-07-20 20:59:50 -0400

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def index
+  end
+  def create
+   @user = User.create(email: params[:email], age: params[:age], comment: params[:comment])
+  end
+  def dashboard
+    if params[:email].present?
+      @users = User.where('email = ?', params[:email])
+    else
+      @users = User.all
+    end
+  end
+  
+end
